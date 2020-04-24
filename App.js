1e149c57d547b749a6853d7c572c7fa6e303e8d8
minciu <stefan_minciuna@yahoo.com>
2020-04-24 20:29:05 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,31 +1,74 @@
-import React, { useState, useEffect } from 'react';
-import "bootstrap/dist/css/bootstrap.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Toast from 'react-bootstrap/Toast';
-import Container from 'react-bootstrap/Container';
-
-import './App.css';
-
-function App() {
-  const[suggestions,setSuggestions]=useState([])
-
-  useEffect(()=>{
-fetch('https://jsonplaceholder.typicode.com/users').then(res=>res.json()).then(data=>{
-setSuggestions(data);
-})
+import React, {
+    useState,
+    useEffect
+  } from 'react';
+  import "bootstrap/dist/css/bootstrap.css";
+  import "slick-carousel/slick/slick.css";
+  import "slick-carousel/slick/slick-theme.css";
+  import './App.css';
+  import Slider from "react-slick";
+  
+  function App() {
+    const [suggestions,setSuggestions]=useState([])
+  
+  
+    useEffect(()=>{
+  fetch('https://jsonplaceholder.typicode.com/users').then(res=>res.json()).then(data=>{
+    setSuggestions(data);
+  })
+    });
+    let settings = {
+      infinite: false,
+      speed: 1000,
+      arrows:true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+  
+      responsive: [
+        {
+          breakpoint: 960,
+          settings: {
+              slidesToShow: 3,
+              slidesToScroll: 2
+          }
+      },
+      {
+          breakpoint: 480,
+          settings: {
+              slidesToShow: 1,
+              slidesToScroll: 2
+          }
+      }
+    ]
+    }
+    
+        return (
+          <div className="container">
+              <h6 className="text-muted">Restaurante</h6>
+              {suggestions.length===0?(
+                  <div className="spinner-border" role="status">
+                      <span className="sr-only">Loading...</span>
+                  </div>
+              ):(
+                  <Slider {...settings}>
+                  {suggestions.map(current=>(
+                          <div className="out" key={current.id}>
+                              <div className="card">
+                                  <img className="rounded-circle" alt={"names here"} src={`https://source.unsplash.com/random/${current.id}`} height={56} width={56} />
+                                  <div className="card-body">
+                                  <h5 className="card-title">{current.email}</h5>
+                                      <small className="card-text text-sm-center text-muted">Restaurant</small>
+                                      <br/>
+                                      <button className="btn btn-sm follow btn-primary">Viziteaza</button>
+                                  </div>
+                              </div>
+                          </div>
+                      ))}
+                  </Slider>
+              )}
+          </div>
+      );
   }
-  );
-  return(
-    <div className="container">
-      <h6 className="text-muted">Restaurants</h6>
-      
-
-      </div>
-  )
-
-}
-
-export default App;
+  
+  export default App;
+  
